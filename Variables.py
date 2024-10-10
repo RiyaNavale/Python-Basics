bf62e1df04ba3e731a5a47b1f26e2b9ff464b0dc
RiyaNavale <riya.navale2010@gmail.com>
2024-10-09 20:36:37 -0400

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,10 @@
+print('\n---- Variables ----\n')
+
+person_name='Alice'
+age=17
+fav_color='Blue'
+fav_food='Pizza'
+hobby='Singing'
+fav_animal='Bunny'
+
+print(f" Hi my name is {person_name} and I am {age} years old. My favorite color is {fav_color} and a hobby of mine is {hobby}. And lastly my favorite animal is a {fav_animal}")
